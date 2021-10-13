f2b10e1db53fa1d0b18cd32c9fe4e451b38e4296
devulogi <devulogijrp.github@gmail.com>
2021-10-13 15:56:58 +0800

    created constructor and tested

---
+++ test/Constructor.Pattern.spec.js
@@ -0,0 +1,26 @@
+'use strict';
+
+const { expect } = require('chai');
+
+const Todo = function (task) {
+  this.task = task;
+  this.completed = false;
+  this.updateTaskStatus = function () {
+    this.completed = !this.completed;
+  };
+};
+
+describe('constructor pattern', () => {
+  it('should create instance of Todo Class', done => {
+    const todo = new Todo();
+    expect(todo).to.be.instanceOf(Todo);
+    done();
+  });
+
+  it('should update status of todo object', done => {
+    const todo = new Todo();
+    todo.updateTaskStatus();
+    expect(todo.completed).to.be.true;
+    done();
+  });
+});
