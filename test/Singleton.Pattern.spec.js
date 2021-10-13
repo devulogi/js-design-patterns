bebdbfc00b99b4051539139d7ac4104a69e5dbe5
devulogi <devulogijrp.github@gmail.com>
2021-10-13 18:56:21 +0800

    singleton created and tested

---
+++ test/Singleton.Pattern.spec.js
@@ -0,0 +1,52 @@
+const { expect } = require('chai');
+const { Factory } = require('./Factory.Pattern.spec');
+
+const Singleton = function () {
+  const tasks = {
+    urgent: [],
+    not_important: [],
+  };
+  return {
+    createTasks: function ({ type, task, createdAt }) {
+      switch (type) {
+        case 'urgent':
+          tasks.urgent.push({
+            task: task.task,
+            completed: task.completed,
+            createdAt,
+          });
+          return this;
+        case 'not_important':
+          tasks.not_important.push({
+            task: task.task,
+            completed: task.completed,
+            createdAt,
+          });
+          return this;
+        default:
+          throw 'No params provided.';
+      }
+    },
+    getUrgentTasks: function () {
+      return tasks.urgent;
+    },
+    getNotImportantTasks: function () {
+      return tasks.not_important;
+    },
+    getTaskCollection: function () {
+      return tasks;
+    },
+  };
+};
+
+describe('singleton pattern', () => {
+  it('should return urgent task', done => {
+    const taskCollection = Singleton()
+      .createTasks(new Factory('urgent', 'fix bugs #141'))
+      .createTasks(new Factory('urgent', 'fix bugs #143'))
+      .createTasks(new Factory('not_important', 'play games'))
+      .createTasks(new Factory('not_important', 'watch anime'));
+    expect(taskCollection.getUrgentTasks()[0].task).to.equal('fix bugs #141');
+    done();
+  });
+});
